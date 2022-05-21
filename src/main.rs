4a54954c62fc68ec6edbc74e49f5f92272b8256c
Nate (1x2kb) <benhjahminb.college@gmail.com>
2022-05-21 09:50:09 -0500

    Chapter 2

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,17 @@ pub const CLEAR: Color = Color::rgb(0.1, 0.1, 0.1);
 pub const RESOLUTION: f32 = 16.0 / 9.0;
 pub const TILE_SIZE: f32 = 0.1;
 
-struct AsciiSheet(Handle<TextureAtlas>);
-
+mod debug;
 mod player;
+mod tilemap;
+
+mod ascii;
+
+use ascii::AsciiPlugin;
 use player::{Player, PlayerPlugin};
+use tilemap::TileMapPlugin;
+
+use debug::DebugPlugin;
 
 fn main() {
     let height = 900.0;
@@ -25,8 +32,10 @@ fn main() {
         })
         .add_plugins(DefaultPlugins)
         .add_startup_system(spawn_camera)
-        .add_startup_system_to_stage(StartupStage::PreStartup, load_ascii)
         .add_plugin(PlayerPlugin)
+        .add_plugin(DebugPlugin)
+        .add_plugin(AsciiPlugin)
+        .add_plugin(TileMapPlugin)
         .run();
 }
 
@@ -42,17 +51,4 @@ fn spawn_camera(mut commands: Commands) {
     camera.orthographic_projection.scaling_mode = ScalingMode::None;
 
     commands.spawn_bundle(camera);
-}
-
-fn load_ascii(
-    mut commands: Commands,
-    assets: Res<AssetServer>,
-    mut texture_atlases: ResMut<Assets<TextureAtlas>>,
-) {
-    let image: Handle<Image> = assets.load("Ascii.png");
-    let atlas =
-        TextureAtlas::from_grid_with_padding(image, Vec2::splat(9.0), 16, 16, Vec2::splat(2.0));
-
-    let atlas_handle = texture_atlases.add(atlas);
-    commands.insert_resource(AsciiSheet(atlas_handle));
 }
--- src/player.rs
+++ src/player.rs
@@ -1,10 +1,15 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, sprite::collide_aabb::collide};
+use bevy_inspector_egui::Inspectable;
 
-use crate::{AsciiSheet, TILE_SIZE};
+use crate::{
+    ascii::{spawn_ascii_sprite, AsciiSheet},
+    tilemap::TileCollider,
+    TILE_SIZE,
+};
 
 pub struct PlayerPlugin;
 
-#[derive(Component)]
+#[derive(Component, Inspectable)]
 pub struct Player {
     speed: f32,
 }
@@ -12,46 +17,90 @@ pub struct Player {
 impl Plugin for PlayerPlugin {
     fn build(&self, a: &mut App) {
         a.add_startup_system(spawn_player)
-            .add_system(player_movement);
+            .add_system(camera_follow.after("movement"))
+            .add_system(player_movement.label("movement"));
     }
 }
 
 fn player_movement(
     mut player_query: Query<(&Player, &mut Transform)>,
+    wall_query: Query<&Transform, (With<TileCollider>, Without<Player>)>,
     keyboard: Res<Input<KeyCode>>,
     time: Res<Time>,
 ) {
     let (player, mut transform) = player_query.single_mut();
 
+    let mut y_delta = 0.0;
     if keyboard.pressed(KeyCode::W) {
-        transform.translation.y += 0.1 * player.speed * time.delta_seconds();
+        // y_delta = translation.y += 0.1 * player.speed * time.delta_seconds();
+        y_delta += player.speed * TILE_SIZE * time.delta_seconds();
     }
     if keyboard.pressed(KeyCode::S) {
-        transform.translation.y -= 0.1 * player.speed * time.delta_seconds();
+        y_delta -= player.speed * TILE_SIZE * time.delta_seconds();
     }
+
+    let mut x_delta = 0.0;
     if keyboard.pressed(KeyCode::A) {
-        transform.translation.x -= 0.1 * player.speed * time.delta_seconds();
+        // transform.translation.x -= 0.1 * player.speed * time.delta_seconds();
+        x_delta -= player.speed * TILE_SIZE * time.delta_seconds()
     }
     if keyboard.pressed(KeyCode::D) {
-        transform.translation.x += 0.1 * player.speed * time.delta_seconds();
+        x_delta += player.speed * TILE_SIZE * time.delta_seconds()
+    }
+
+    let target = transform.translation + Vec3::new(x_delta, 0.0, 0.0);
+    if wall_collision_check(target, &wall_query) {
+        transform.translation = target;
+    }
+
+    let target = transform.translation + Vec3::new(0.0, y_delta, 0.0);
+    if wall_collision_check(target, &wall_query) {
+        transform.translation = target;
     }
 }
 
-fn spawn_player(mut commands: Commands, ascii: Res<AsciiSheet>) {
-    let mut sprite = TextureAtlasSprite::new(1);
-    sprite.color = Color::rgb(0.3, 0.3, 0.9);
-    sprite.custom_size = Some(Vec2::splat(TILE_SIZE));
+fn camera_follow(
+    player_query: Query<&Transform, With<Player>>,
+    mut camera_query: Query<&mut Transform, (With<Camera>, Without<Player>)>,
+) {
+    let player_transform = player_query.single();
+    let mut camera_transform = camera_query.single_mut();
 
-    let player = commands
-        .spawn_bundle(SpriteSheetBundle {
-            sprite: sprite,
-            texture_atlas: ascii.0.clone(),
-            transform: Transform {
-                translation: Vec3::new(0.0, 0.0, 900.0),
-                ..Default::default()
-            },
-            ..Default::default()
-        })
+    camera_transform.translation.x = player_transform.translation.x;
+    camera_transform.translation.y = player_transform.translation.y;
+}
+
+fn wall_collision_check(
+    target_player_pos: Vec3,
+    wall_query: &Query<&Transform, (With<TileCollider>, Without<Player>)>,
+) -> bool {
+    for wall_transform in wall_query.iter() {
+        let collision = collide(
+            target_player_pos,
+            Vec2::splat(TILE_SIZE * 0.9),
+            wall_transform.translation,
+            Vec2::splat(TILE_SIZE),
+        );
+
+        if collision.is_some() {
+            return false;
+        }
+    }
+
+    true
+}
+
+fn spawn_player(mut commands: Commands, ascii: Res<AsciiSheet>) {
+    let player = spawn_ascii_sprite(
+        &mut commands,
+        &ascii,
+        1,
+        Color::rgb(0.3, 0.3, 0.9),
+        Vec3::new(2.0 * TILE_SIZE, -2.0 * TILE_SIZE, 900.0),
+    );
+
+    commands
+        .entity(player)
         .insert(Name::new("Player"))
         .insert(Player { speed: 3.0 })
         .id();
@@ -60,16 +109,16 @@ fn spawn_player(mut commands: Commands, ascii: Res<AsciiSheet>) {
     background_sprite.color = Color::rgb(0.5, 0.5, 0.5);
     background_sprite.custom_size = Some(Vec2::splat(TILE_SIZE));
 
-    let background = commands
-        .spawn_bundle(SpriteSheetBundle {
-            sprite: background_sprite,
-            texture_atlas: ascii.0.clone(),
-            transform: Transform {
-                translation: Vec3::new(0.0, 0.0, -1.0),
-                ..Default::default()
-            },
-            ..Default::default()
-        })
+    let background = spawn_ascii_sprite(
+        &mut commands,
+        &ascii,
+        0,
+        Color::rgb(0.5, 0.5, 0.5),
+        Vec3::new(0.0, 0.0, -1.0),
+    );
+
+    commands
+        .entity(background)
         .insert(Name::new("Background"))
         .id();
 
